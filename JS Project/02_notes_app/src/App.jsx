5f5399a255dcf48fbe25b20035f4c9aa7a2e02f3
Anjish7 <anjish9122364264@gmail.com>
2026-02-15 15:18:00 +0530

    update notes app

--- JS Project/02_notes_app/src/App.jsx
+++ JS Project/02_notes_app/src/App.jsx
@@ -15,6 +15,14 @@ function App() {
     setDetails("");
     setTitle("");
   };
+  const deleteNote=(idx)=>{
+    const copyTask=[...task]
+    copyTask.splice(idx,1)
+    setTask(copyTask)
+
+
+
+  }
   return (
     <div className="min-h-screen bg-black text-white p-10">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
@@ -47,7 +55,7 @@ function App() {
               className="w-full h-40 bg-transparent border-2 border-white rounded-xl px-4 py-3 outline-none resize-none"
             ></textarea>
 
-            <button className="w-full bg-white text-black py-3 rounded-xl font-semibold hover:opacity-90">
+            <button className="w-full active:scale-95 bg-white text-black py-3 rounded-xl font-semibold hover:opacity-90">
               Add Note
             </button>
           </form>
@@ -58,11 +66,24 @@ function App() {
           <h1 className="text-4xl font-bold mb-8">Recent Notes</h1>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-            {task.map((el,idx) => {
+            {task.map((el, idx) => {
               return (
-                <div key={idx} className="bg-white text-black p-5 rounded-2xl shadow-lg   ">
-                  <h2 className="font-bold text-lg">{el.title}</h2>
-                  <p className="text-sm mt-2">{el.details}</p>
+                <div
+                  key={idx}
+                  className=" text-black p-5 rounded-2xl shadow-lg  flex flex-col justify-between w-2xs h-[230px] bg-[url(https://png.pngtree.com/png-vector/20230102/ourmid/pngtree-sticky-pastel-notes-aesthetic-smooth-color-paper-png-image_6548936.png)] bg-cover"
+                >
+                  <div className="text-center">
+                    <h2 className="font-bold mt-8  text-3xl">{el.title}</h2>
+                    <p className="text-2xl text-gray-400 mt-2">{el.details}</p>
+                  </div>
+                  <div className="flex justify-center">
+                    <button className="bg-red-500 active:scale-90 cursor-pointer text-white w-[120px] rounded-2xl "
+                     onClick={()=>{
+                      deleteNote(idx)
+                     }}
+                    >Delete Note</button>
+                  </div>
+
                 </div>
               );
             })}
